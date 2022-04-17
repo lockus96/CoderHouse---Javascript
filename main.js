5cd17683dd08dd1f06411f6a5cbd11019cb7e435
Miguel Angel <angelmcarrizo@gmail.com>
2022-04-16 21:05:08 -0300

    modif 16/4

--- main.js
+++ main.js
@@ -1,80 +1,111 @@
-class Personas {
-  constructor(nombre, prestamo, motivo, cuotas) {
-    this.nombre = nombre;
-    this.prestamo = parseFloat(prestamo);
-    this.motivo = motivo;
-    this.cuotas = parseFloat(cuotas);
-  }
+class Persona {
+	constructor(nombre, prestamo, motivo, cuotas) {
+		this.nombre = nombre;
+		this.prestamo = parseFloat(prestamo);
+		this.motivo = motivo;
+		this.cuotas = parseFloat(cuotas);
+	}
 
-  interesTotal() {
-    return Math.round(this.prestamo * 1.3)
-  }
-  montoCuotas() {
-    return Math.round(this.interesTotal() / this.cuotas)
-  }
+	interesTotal() {
+		return Math.round(this.prestamo * 1.3);
+	}
+	montoCuotas() {
+		return Math.round(this.interesTotal() / this.cuotas);
+	}
 }
 
-// Constructor de objetos con datos del JSON recuperados
-class datosPersona {
-  constructor(obj) {
-      this.nombre  = obj.nombre;
-      this.prestamo  = parseFloat(obj.prestamo);
-      this.motivo  = obj.motivo;
-      this.cuotas  = parseFloat(obj.cuotas);
-  }
 
-  //arreglar esto porque no funciona
-  interesTotal() {
-    return Math.round(this.prestamo * 1.3)
-  }
-  montoCuotas() {
-    return Math.round(this.interesTotal() / this.cuotas)
-  }
+class DatosPersona {
+	constructor(obj) {
+		this.nombre = obj.nombre;
+		this.prestamo = parseFloat(obj.prestamo);
+		this.motivo = obj.motivo;
+		this.cuotas = parseFloat(obj.cuotas);
+	}
+
+
+	interesTotal() {
+		return Math.round(this.prestamo * 1.3);
+	}
+	montoCuotas() {
+		return Math.round(this.interesTotal() / this.cuotas);
+	}
 }
+
+
+let btnRegistro = document.querySelector("#registro");
+btnRegistro.addEventListener("click", () => obtenerInput());
+
+let btnVisualizar = document.querySelector("#visualizar");
+btnVisualizar.addEventListener("click", () => recorrerDatos());
+
 
 
 let arrayPersonas = [];
 
-const guardarLocal = (clave, valor) => {
-  localStorage.setItem(clave, valor)
-};
 
-const datosGuardados = JSON.parse(localStorage.getItem("listaClientes"));
-let datosRecuperados = [];
+let datosRecuperados = JSON.parse(localStorage.getItem("listaClientes"));
 
-
-
-const obtenerInput = () => {
-
-  let nombreCliente = document.querySelector("#valorNombre").value;
-  let prestamoTotal = document.querySelector("#valorPrestamo").value;
-  let motivoPrestamo = document.querySelector("#valorMotivo").value;
-  let cantidadCuotas = document.querySelector("#valorCuotas").value;
-  arrayPersonas.push(new Personas(nombreCliente, prestamoTotal, motivoPrestamo, cantidadCuotas));
-  guardarLocal("listaClientes", JSON.stringify(arrayPersonas));
-  for (const objeto of datosGuardados){
-    datosRecuperados.push(new datosPersona(objeto));
-  }
-
+if (datosRecuperados) {
+	for (const persona of datosRecuperados) {
+		arrayPersonas.push(new DatosPersona(persona));
+	}
 }
 
-const recorrerDatos = () => {
+const guardarLocal = (clave, valor) => {
+	localStorage.setItem(clave, valor);
+};
 
-  let container = document.querySelector("#cajaResultados");
-  container.innerHTML = "";
+const obtenerInput = () => {
+	let nombreCliente = document.querySelector("#valorNombre").value;
+	let prestamoTotal = document.querySelector("#valorPrestamo").value;
+	let motivoPrestamo = document.querySelector("#valorMotivo").value;
+	let cantidadCuotas = document.querySelector("#valorCuotas").value;
 
-  for(const personas of datosRecuperados){
-    container = document.createElement("div");
-    container.innerHTML = `        
+	arrayPersonas.push(new Persona(nombreCliente, prestamoTotal, motivoPrestamo, cantidadCuotas));
+	guardarLocal("listaClientes", JSON.stringify(arrayPersonas));
+
+	//cumple la funcion de evitar un doble clic
+	let timerInterval
+	Swal.fire({
+		title: 'Guardando datos',
+		html: 'Solo tomará un momento...',
+		timer: 1000,
+		timerProgressBar: true,
+		didOpen: () => {
+			Swal.showLoading()
+			const b = Swal.getHtmlContainer().querySelector('b')
+			timerInterval = setInterval(() => {
+				b.textContent = Swal.getTimerLeft()
+			}, 100)
+		},
+		willClose: () => {
+			clearInterval(timerInterval)
+		}
+	}).then((result) => {
+		if (result.dismiss === Swal.DismissReason.timer) {
+			console.log('Funciono bien jeje')
+		}
+	})
+
+};
+
+function recorrerDatos() {
+	let container = document.querySelector("#cajaResultados");
+	container.innerHTML = "";
+	for (const persona of arrayPersonas) {
+		container = document.createElement("div");
+		container.innerHTML = `        
     <div class="card text-white bg-dark mb-3" style="width: 18rem;">
         <div class="card-body">
-        <h5 class="card-title">Nombre: ${personas.nombre}</h5>
-        <p class="card-text">Motivo: ${personas.motivo}</p>
-        <p class="card-text">Prestamo: ${personas.prestamo}</p>
-        <p class="card-text">C/Interes: ${personas.interesTotal()}</p>
-        <p class="card-text">Cantidad Cuotas: ${personas.cuotas}</p>
-        <p class="card-text">Cuotas: ${personas.montoCuotas()}</p>
-    </div>`
-    document.querySelector("#cajaResultados").appendChild(container);
-  }
+        <h5 class="card-title">Nombre: ${persona.nombre}</h5>
+        <p class="card-text">Motivo: ${persona.motivo}</p>
+        <p class="card-text">Prestamo: ${persona.prestamo}</p>
+        <p class="card-text">C/Interes: ${persona.interesTotal()}</p>
+        <p class="card-text">Cantidad Cuotas: ${persona.cuotas}</p>
+        <p class="card-text">Cuotas: ${persona.montoCuotas()}</p>
+    </div>`;
+
+		document.querySelector("#cajaResultados").appendChild(container);
+	}
 }
